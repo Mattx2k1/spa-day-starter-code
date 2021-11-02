a9464e5f6631ea7a47e3be455aa39e996a5768dd
Mattx2k1 <44537080+Mattx2k1@users.noreply.github.com>
2021-11-01 20:54:43 -0400

    almost done w/ Processing Form Submission

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,7 +1,11 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,6 +15,19 @@ public class UserController {
     @GetMapping("add")
     public String displayAddUserForm(){
         return "user/add";
+    }
+
+    @PostMapping("")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        model.addAttribute("user", user);
+        if (user.getPassword().equals(verify)) {
+            return "user/index";
+
+        } else {
+            return "user/add";
+        }
+
+
     }
 
 
